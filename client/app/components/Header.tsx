b84ad396aaaa42d9a396427f7cbb79bcc689cc0a
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-11-25 16:40:13 +0530

    Implementing Social Auth Feature

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Link from "next/link";
 import NavItems from "@/app/utils/NavItems";
 import { ThemeSwitcher } from "@/app/utils/ThemeSwitcher";
@@ -10,6 +10,9 @@ import Verification from "./Auth/Verification";
 import { useSelector } from "react-redux";
 import Image from "next/image";
 import avatar from "@/public/assets/avatar.png";
+import { useSession } from "next-auth/react";
+import { useSocialAuthMutation } from "@/redux/features/auth/authApi";
+import { toast } from "react-hot-toast";
 
 type Props = {
   open: boolean;
@@ -22,6 +25,24 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, setRoute, open }) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
   const { user } = useSelector((state: any) => state.auth);
+  const { data } = useSession();
+  const [socialAuth, { isSuccess, error }] = useSocialAuthMutation();
+
+  useEffect(() => {
+    if (!user) {
+      if (data) {
+        socialAuth({
+          email: data?.user?.email,
+          name: data?.user?.name,
+          avatar: data?.user?.image,
+        });
+      }
+    }
+
+    if (isSuccess) {
+      toast.success("Login Successfully!");
+    }
+  }, [data, user, isSuccess]);
 
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", () => {
